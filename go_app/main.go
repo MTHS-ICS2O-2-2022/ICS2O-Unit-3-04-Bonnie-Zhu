b05484ed6c5fde66aae1d106daea349100afde1e
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-04-21 02:47:15 +0000

    done

---
+++ go_app/main.go
@@ -0,0 +1,34 @@
+// Created by: Bonnie ZHu
+// Created on: April 2023
+//
+// This program shows conversion from Fahrenheit to Celsius
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// This function shows conversion from Fahrenheit to Celsius
+
+	var fahrenheit float64
+	var celsius float64
+
+	// input
+	fmt.Println("This program shows conversion from Fahrenheit to Celsius")
+	fmt.Println()
+	fmt.Print("Enter Fahrenheit: ")
+	fmt.Scanln(&fahrenheit)
+
+	// process
+	celsius = (fahrenheit - 32) * 5 / 9
+	celsius = math.Round(celsius*100) / 100 // round to 2 decimal places
+
+	// output
+	fmt.Println()
+	fmt.Printf("Converted to Celsius: %.2f ℃.\n", celsius)
+	fmt.Println()
+	fmt.Println("Done.")
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Zhu
+// Created on: April. 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  // input
+  const fahrenheit = parseInt(document.getElementById("fahrenheit ").value)
+  const zeroDegrees = 32
+  const standard = 5 / 9
+
+  // process
+  const conversion = (fahrenheit - zeroDegrees) * standard
+
+  // output
+  document.getElementById("conversion").innerHTML = "converted to celsius is: " + conversion.toFixed(2) + "°C"
+}
